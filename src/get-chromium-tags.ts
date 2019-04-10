f3a4038646c632cf8aa91dc8f15548a835980ae4
Jeremy Apthorp <nornagon@nornagon.net>
2019-04-10 14:29:04 -0400

    refactor: factor out getJSON

--- src/get-chromium-tags.ts
+++ src/get-chromium-tags.ts
@@ -1,19 +1,27 @@
 import * as https from 'https';
 
-export function getChromiumTags() {
+function get(url: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    https.get('https://chromium.googlesource.com/chromium/src/+refs/tags?format=JSON', (res) => {
+    https.get(url, (res) => {
       let s = '';
       res.on('data', (d) => {
         s += d.toString('utf8');
       });
       res.on('end', () => {
-        resolve(JSON.parse(s.slice(s.indexOf('{'))));
+        resolve(s);
       });
     }).on('error', (e) => {
       reject(e);
     });
   });
+}
+
+function getJSON(url: string): Promise<any> {
+  return get(url).then(s => JSON.parse(s.slice(s.indexOf('{'))))
+}
+
+export function getChromiumTags(): Promise<object> {
+  return getJSON('https://chromium.googlesource.com/chromium/src/+refs/tags?format=JSON')
 }
 
 export interface ChromiumCommit {
@@ -34,17 +42,5 @@ export interface ChromiumCommit {
 }
 
 export function getChromiumCommits(fromRef: string, toRef: string): Promise<{log: ChromiumCommit[], next?: string}> {
-  return new Promise((resolve, reject) => {
-    https.get(`https://chromium.googlesource.com/chromium/src/+log/${fromRef}..${toRef}?format=JSON`, (res) => {
-      let s = '';
-      res.on('data', (d) => {
-        s += d.toString('utf8');
-      });
-      res.on('end', () => {
-        resolve(JSON.parse(s.slice(s.indexOf('{'))));
-      });
-    }).on('error', (e) => {
-      reject(e);
-    });
-  });
+  return getJSON(`https://chromium.googlesource.com/chromium/src/+log/${fromRef}..${toRef}?format=JSON`)
 }
