009075cdb5dc61f9ac2f2b0ed49f545f4517c7aa
John Kleinschmidt <jkleinsc@github.com>
2020-06-04 11:48:52 -0400

    fix: update supported branch pattern (#49)
    Update to handle 2 digit versions

--- src/handlers.ts
+++ src/handlers.ts
@@ -11,18 +11,28 @@ import { roll } from './utils/roll';
 export function getSupportedBranches(branches: { name: string }[]): string[] {
   const releaseBranches = branches
     .filter(branch => {
-      const releasePattern = /^[0-9]+-([0-9]+-x|x-y)$/;
+      const releasePattern = /^(\d)+-(?:(?:[0-9]+-x$)|(?:x+-y$))$/;
       return releasePattern.test(branch.name);
     })
     .map(b => b.name);
 
   const filtered: Record<string, string> = {};
-  releaseBranches.sort().forEach((branch: string) => {
-    return (filtered[branch.split('-')[0]] = branch);
-  });
+  releaseBranches
+    .sort((a, b) => {
+      const aParts = a.split('-');
+      const bParts = b.split('-');
+      for (let i = 0; i < aParts.length; i += 1) {
+        if (aParts[i] === bParts[i]) continue;
+        return parseInt(aParts[i], 10) - parseInt(bParts[i], 10);
+      }
+      return 0;
+    })
+    .forEach(branch => {
+      return (filtered[branch.split('-')[0]] = branch);
+    });
 
   const values = Object.values(filtered);
-  return values.sort().slice(-NUM_SUPPORTED_VERSIONS);
+  return values.sort((a, b) => parseInt(a, 10) - parseInt(b, 10)).slice(-NUM_SUPPORTED_VERSIONS);
 }
 
 export async function handleChromiumCheck(): Promise<void> {
