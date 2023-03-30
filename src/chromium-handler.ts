4f3b0b3dd1fc65bf1f3929ae0ebb67ae965a557f
Shelley Vohr <shelley.vohr@gmail.com>
2023-03-30 16:40:12 +0200

    refactor: split out rollReleaseBranch

--- src/chromium-handler.ts
+++ src/chromium-handler.ts
@@ -2,13 +2,82 @@ import * as debug from 'debug';
 
 import { MAIN_BRANCH, REPOS, ROLL_TARGETS } from './constants';
 import { compareChromiumVersions } from './utils/compare-chromium-versions';
-import { getChromiumReleases } from './utils/get-chromium-tags';
+import { getChromiumReleases, Release } from './utils/get-chromium-tags';
 import { getSupportedBranches } from './utils/get-supported-branches';
 import { getOctokit } from './utils/octokit';
 import { roll } from './utils/roll';
 import { ReposListBranchesResponseItem } from './types';
+import { Octokit } from '@octokit/rest';
 
-export async function handleChromiumCheck(target?: string): Promise<void> {
+async function rollReleaseBranch(
+  github: Octokit,
+  branch: ReposListBranchesResponseItem,
+  chromiumReleases: Release[],
+) {
+  const d = debug(`roller/chromium:rollReleaseBranch('${branch}')`);
+
+  d(`Fetching DEPS for ${branch.name}`);
+  const { data: depsData } = await github.repos.getContent({
+    ...REPOS.electron,
+    path: 'DEPS',
+    ref: branch.commit.sha,
+  });
+
+  if (!('content' in depsData)) {
+    throw new Error(`Error - incorrectly got array when fetching DEPS content for ${branch}`);
+  }
+
+  const deps = Buffer.from(depsData.content, 'base64').toString('utf8');
+  const versionRegex = new RegExp(`${ROLL_TARGETS.chromium.depsKey}':\n +'(.+?)',`, 'm');
+  const [, chromiumVersion] = versionRegex.exec(deps);
+
+  const chromiumMajorVersion = Number(chromiumVersion.split('.')[0]);
+
+  // We should be able to parse major version as a number.
+  if (Number.isNaN(chromiumMajorVersion)) {
+    const SHAPattern = /\b[0-9a-f]{5,40}\b/;
+    // On newer release branches we may not yet have updated the branch to use tags
+    if (`${chromiumMajorVersion}`.match(SHAPattern)) {
+      throw new Error(`${branch.name} roll failed: ${chromiumMajorVersion} should be a tag.`);
+    } else {
+      throw new Error(
+        `${branch.name} roll failed: ${chromiumVersion} is not a valid version number`,
+      );
+    }
+  }
+
+  d(`Computing latest upstream version for Chromium ${chromiumMajorVersion}`);
+  const upstreamVersions = chromiumReleases
+    .filter(
+      r =>
+        ['Win32', 'Windows', 'Linux', 'Mac'].includes(r.platform) &&
+        r.milestone === chromiumMajorVersion,
+    )
+    .sort((a, b) => a.time - b.time)
+    .map(r => r.version);
+  const latestUpstreamVersion = upstreamVersions[upstreamVersions.length - 1];
+  if (
+    latestUpstreamVersion &&
+    compareChromiumVersions(latestUpstreamVersion, chromiumVersion) > 0
+  ) {
+    d(
+      `Upgrade possible: ${branch.name} can roll from ${chromiumVersion} to ${latestUpstreamVersion}`,
+    );
+    try {
+      await roll({
+        rollTarget: ROLL_TARGETS.chromium,
+        electronBranch: branch,
+        targetVersion: latestUpstreamVersion,
+      });
+    } catch (e) {
+      throw new Error(`Error rolling ${branch.name} to ${latestUpstreamVersion}: ${e.message}`);
+    }
+  } else {
+    d(`No upgrade found, ${chromiumVersion} is the most recent known in its release line.`);
+  }
+}
+
+export async function handleChromiumCheck(): Promise<void> {
   const d = debug('roller/chromium:handleChromiumCheck()');
   d('Fetching Chromium releases');
   const chromiumReleases = await getChromiumReleases();
@@ -30,68 +99,7 @@ export async function handleChromiumCheck(target?: string): Promise<void> {
 
   // Roll all non-main release branches.
   for (const branch of releaseBranches) {
-    d(`Fetching DEPS for ${branch.name}`);
-    const { data: depsData } = await github.repos.getContent({
-      ...REPOS.electron,
-      path: 'DEPS',
-      ref: branch.commit.sha,
-    });
-
-    if (!('content' in depsData)) {
-      d(`Error - incorrectly got array when fetching DEPS content for ${branch}`);
-      thisIsFine = false;
-      continue;
-    }
-
-    const deps = Buffer.from(depsData.content, 'base64').toString('utf8');
-    const versionRegex = new RegExp(`${ROLL_TARGETS.chromium.depsKey}':\n +'(.+?)',`, 'm');
-    const [, chromiumVersion] = versionRegex.exec(deps);
-
-    const chromiumMajorVersion = Number(chromiumVersion.split('.')[0]);
-
-    // We should be able to parse major version as a number.
-    if (Number.isNaN(chromiumMajorVersion)) {
-      const SHAPattern = /\b[0-9a-f]{5,40}\b/;
-      // On newer release branches we may not yet have updated the branch to use tags
-      if (`${chromiumMajorVersion}`.match(SHAPattern)) {
-        d(`${branch.name} roll failed: ${chromiumMajorVersion} should be a tag.`);
-      } else {
-        d(`${branch.name} roll failed: ${chromiumVersion} is not a valid version number`);
-      }
-      thisIsFine = false;
-      continue;
-    }
-
-    d(`Computing latest upstream version for Chromium ${chromiumMajorVersion}`);
-    const upstreamVersions = chromiumReleases
-      .filter(
-        r =>
-          ['Win32', 'Windows', 'Linux', 'Mac'].includes(r.platform) &&
-          r.milestone === chromiumMajorVersion,
-      )
-      .sort((a, b) => a.time - b.time)
-      .map(r => r.version);
-    const latestUpstreamVersion = upstreamVersions[upstreamVersions.length - 1];
-    if (
-      latestUpstreamVersion &&
-      compareChromiumVersions(latestUpstreamVersion, chromiumVersion) > 0
-    ) {
-      d(
-        `Upgrade possible: ${branch.name} can roll from ${chromiumVersion} to ${latestUpstreamVersion}`,
-      );
-      try {
-        await roll({
-          rollTarget: ROLL_TARGETS.chromium,
-          electronBranch: branch,
-          targetVersion: latestUpstreamVersion,
-        });
-      } catch (e) {
-        d(`Error rolling ${branch.name} to ${latestUpstreamVersion}: `, e);
-        thisIsFine = false;
-      }
-    } else {
-      d(`No upgrade found, ${chromiumVersion} is the most recent known in its release line.`);
-    }
+    await rollReleaseBranch(github, branch, chromiumReleases);
   }
 
   d(`Fetching ${MAIN_BRANCH} branch for electron/electron`);
