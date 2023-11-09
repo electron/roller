9651f258b374abae122b3b03bbfc1ebfb61acdc6
George Xu <george.xu@slack-corp.com>
2023-11-09 12:17:48 -0800

    change to cron

--- src/orb-cron.ts
+++ src/orb-cron.ts
@@ -2,7 +2,7 @@ import { rollMainBranch } from './orb-handler';
 
 if (require.main === module) {
   rollMainBranch().catch((err: Error) => {
-    console.log('Orb Roll Failed');
+    console.log('Orb Cron Failed');
     console.error(err);
     process.exit(1);
   });
