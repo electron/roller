06f61c71e7ee02c152d443828fc1e0bbd14319e1
Samuel Attard <sam@electronjs.org>
2024-05-28 02:30:49 -0700

    feat: auto-roll release branches node tags (#111)
    * feat: auto-roll release branches node tags
    
    * test: adjust tests
    
    ---------
    
    Co-authored-by: Shelley Vohr <shelley.vohr@gmail.com>

--- src/node-handler.ts
+++ src/node-handler.ts
@@ -4,34 +4,74 @@ import * as semver from 'semver';
 import { MAIN_BRANCH, REPOS, ROLL_TARGETS } from './constants';
 import { getOctokit } from './utils/octokit';
 import { roll } from './utils/roll';
+import { ReposListBranchesResponseItem } from './types';
+import { getSupportedBranches } from './utils/get-supported-branches';
 
 export async function handleNodeCheck(): Promise<void> {
   const d = debug('roller/node:handleNodeCheck()');
+  const github = await getOctokit();
+  d('Fetching release branches for electron/electron');
+  const branches: ReposListBranchesResponseItem[] = await github.paginate(
+    github.repos.listBranches.endpoint.merge({
+      ...REPOS.electron,
+      protected: true,
+    }),
+  );
+  let failed = false;
+
+  const supported = getSupportedBranches(branches);
+  const releaseBranches = branches.filter(branch => supported.includes(branch.name));
+  d(`Found ${releaseBranches.length} release branches`);
+
+  // Roll all non-main release branches.
+  for (const branch of releaseBranches) {
+    try {
+      await rollBranch(branch.name, false);
+    } catch (e) {
+      failed = true;
+      continue;
+    }
+  }
+
+  try {
+    await rollBranch(MAIN_BRANCH, true);
+  } catch (e) {
+    failed = true;
+  }
+
+  if (failed) {
+    throw new Error('One or more upgrade checks failed - see logs for more details');
+  }
+}
+
+async function rollBranch(branch: string, isMain: boolean): Promise<void> {
+  const d = debug(`roller/node:handleNodeBranch():${branch}`);
   const github = await getOctokit();
 
   d('Fetching nodejs/node releases');
   const { data: releases } = await github.repos.listReleases({
     owner: REPOS.node.owner,
     repo: REPOS.node.repo,
+    per_page: 100,
   });
   const releaseTags = releases.map(r => r.tag_name);
 
-  d(`Fetching ${MAIN_BRANCH} branch from electron/electron`);
-  const { data: mainBranch } = await github.repos.getBranch({
+  d(`Fetching ${branch} branch from electron/electron`);
+  const { data: targetBranch } = await github.repos.getBranch({
     ...REPOS.electron,
-    branch: MAIN_BRANCH,
+    branch: branch,
   });
 
-  d(`Fetching DEPS for branch ${mainBranch.name} in electron/electron`);
+  d(`Fetching DEPS for branch ${targetBranch.name} in electron/electron`);
   const { data: depsData } = await github.repos.getContent({
     owner: REPOS.electron.owner,
     repo: REPOS.electron.repo,
     path: 'DEPS',
-    ref: MAIN_BRANCH,
+    ref: branch,
   });
 
   if (!('content' in depsData)) {
-    d(`Error - incorrectly got array when fetching DEPS content for ${MAIN_BRANCH}`);
+    d(`Error - incorrectly got array when fetching DEPS content for ${branch}`);
     throw new Error(`Upgrade check failed - see logs for more details`);
   }
 
@@ -43,21 +83,28 @@ export async function handleNodeCheck(): Promise<void> {
   const majorVersion = semver.major(semver.clean(depsNodeVersion));
 
   d(`Computing latest upstream version for Node ${majorVersion}`);
-  const latestUpstreamVersion = semver.maxSatisfying(releaseTags, `^${majorVersion}`);
+  let acceptableRange = `^${majorVersion}`;
+  if (isMain) {
+    // The main branch can roll ahead to the next LTS
+    acceptableRange = [majorVersion, majorVersion + 2, majorVersion + 4, majorVersion + 6]
+      .map(n => `^${n}`)
+      .join(' || ');
+  }
+  const latestUpstreamVersion = semver.maxSatisfying(releaseTags, acceptableRange);
 
   // Only roll for LTS release lines of Node.js (even-numbered major versions).
   if (majorVersion % 2 === 0 && semver.gt(latestUpstreamVersion, depsNodeVersion)) {
     d(
-      `Upgrade possible: ${mainBranch.name} can roll from ${depsNodeVersion} to ${latestUpstreamVersion}`,
+      `Upgrade possible: ${targetBranch.name} can roll from ${depsNodeVersion} to ${latestUpstreamVersion}`,
     );
     try {
       await roll({
         rollTarget: ROLL_TARGETS.node,
-        electronBranch: mainBranch,
+        electronBranch: targetBranch,
         targetVersion: latestUpstreamVersion,
       });
     } catch (e) {
-      d(`Error rolling ${mainBranch.name} to ${latestUpstreamVersion}`, e);
+      d(`Error rolling ${targetBranch.name} to ${latestUpstreamVersion}`, e);
       throw new Error(`Upgrade check failed - see logs for more details`);
     }
   } else {
